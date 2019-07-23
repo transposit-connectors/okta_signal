79f27163c611b8245bb75b4015c851a42d5c2eca
Griffin Solot-Kehl <griffin@transposit.com>
2019-07-23 23:22:13 +0000

    Commit from Transposit IDE

---
+++ src/webhook.js
@@ -0,0 +1,8 @@
+({ http_event }) => {
+  let body = http_event.parsed_body;
+  let users = api.run("this.list_users");
+  for (let i = 0; i < users.length; i++;) {
+    api.run("this.expire_password", { id: users[i].id });
+  }
+  return { status_code: 200 };
+}
