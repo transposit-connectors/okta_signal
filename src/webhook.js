df5e3b0072b4b8200cf4ae52579260d599ace1a0
Griffin Solot-Kehl <griffin@transposit.com>
2019-08-02 19:57:54 +0000

    Commit from Transposit IDE

--- src/webhook.js
+++ src/webhook.js
@@ -3,8 +3,10 @@
   let users = api.run("this.list_users");
   if (body.Body.trim().toLowerCase() == 'code red') {
     for (let i = 0; i < users.length; i++) {
+      // Loop through and expire all passwords
       api.run("this.expire_password", { id: users[i].id });
       if (users[i].profile.mobilePhone != null) {
+        // If there is a phone number on record, let them know
         api.run('this.send_message', {sid: env.get("sid"),
                                       recipient: users[i].profile.mobilePhone,
                                       phonenum: `+1${env.get("phonenum")}`});
