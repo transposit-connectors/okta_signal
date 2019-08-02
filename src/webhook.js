d8a01ef66c26ea63415f3229cf128f8a6e1f2703
Griffin Solot-Kehl <griffin@transposit.com>
2019-08-02 19:49:48 +0000

    Commit from Transposit IDE

--- src/webhook.js
+++ src/webhook.js
@@ -4,6 +4,11 @@
   if (body.Body.trim().toLowerCase() == 'code red') {
     for (let i = 0; i < users.length; i++) {
       api.run("this.expire_password", { id: users[i].id });
+      if (users[i].profile.mobilePhone != null) {
+        api.run('this.send_message', {sid: env.get("sid"),
+                                      recipient: users[i].profile.mobilePhone,
+                                      phonenum: env.get("phonenum")});
+      }
   	} 
   }
   return { status_code: 200 };
